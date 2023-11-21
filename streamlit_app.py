e0457141173c1ef374b1d4cc35702f90f29d7304
AYuesuen <151182997+AYuesuen@users.noreply.github.com>
2023-11-21 14:40:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,9 @@ streamlit.header("the fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would like see?', "")
+if streamlit.button('Submit'):
+  insert_query = "INSERT INTO fruit_load_list (FRUIT_NAME) VALUES (?)"
+  data_tuple = (add_my_fruit)
+  my_cur.execute(insert_query, data_tuple)
+
 streamlit.write('thanks for adding', add_my_fruit)
-#my_cur.execute("INSERT INTO fruit_load_list values ()")
